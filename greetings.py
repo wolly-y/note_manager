d0da04619944836fe54e801949390de540c6591c
wolly-y <hsabkram@icloud.com>
2024-12-25 17:52:03 +1100

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,13 @@
+username = 'Mark' # имя пользователя
+title = 'first title' # заголовок заметки
+content = 'first content' # описание заметки
+status = 'active' # статус заметки
+created_date = '25-12-2024' # дата создания заметки
+issue_date = '25-01-2025' # дата истечения заметки
+
+print('Имя пользователя: ', username)
+print('Заголовок заметки: ', title)
+print('Описание заметки: ', content)
+print('Статус заметки: ', status)
+print('Дата создания заметки: ', created_date)
+print('Дата истечения заметки: ', issue_date)
